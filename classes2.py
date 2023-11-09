74a0c1a7e94e53e1613e4fcf341d561f9025d11b
zoufaond <zoufik.o@seznam.cz>
2023-11-09 17:08:31 +0100

    9/11/23

--- classes2.py
+++ classes2.py
@@ -7,28 +7,31 @@ import sys
 import warnings
 
 class SCHR:
-    def __init__(self,phis,alfa,pomery,x,koefs,koefh,a=1,b=1,c=1,d=1,phi0=0,l0s=0,l0h=0):
+    def __init__(self,phis,alfa,x,a=1,b=1,c=1,d=1,phi0=0,l0s=0,l0h=0):
         self.a=a
         self.b=b
         self.c=c
         self.d=d
         self.phi0=phi0
-        self.koefs = koefs
-        self.koefh = koefh
         self.l0s = l0s
         self.l0h = l0h
         self.phis = phis
         self.alfa = alfa
-        self.pomery = pomery
         self.x = x
     
     def geometry_init(self):
-        phih = self.alfa-self.phi0-self.phis
-        xs = sp.sqrt(self.a**2+self.b**2-2*self.a*self.b*sp.cos(self.phis))
-        xh = sp.sqrt(self.c**2+self.d**2-2*self.c*self.d*sp.cos(phih))
-        return xs, xh
+        self.phih = self.alfa-self.phi0-self.phis
+        xs = sp.sqrt(self.a**2+self.b**2-2*self.a*self.b*sp.cos(self.phis))+1
+        xh = sp.sqrt(self.c**2+self.d**2-2*self.c*self.d*sp.cos(self.phih))+1
+        xs_np = sp.lambdify([self.phis,self.alfa],xs, 'numpy')
+        xh_np = sp.lambdify([self.phis,self.alfa],xh, 'numpy')
+        return xs, xh, xs_np, xh_np
     
-    def nonlinear_spring_init(self,akt_f = True,a_fun_coef=1000):
+    def nonlinear_spring_init(self,pomery,koefs,koefh,akt_f = True,a_fun_coef=1000):
+        self.pomery = pomery
+        self.koefs = koefs
+        self.koefh = koefh
+        
         if akt_f:
             a_funs = 1/(1+sp.exp(a_fun_coef*(self.l0s-self.x)))
             a_funh = 1/(1+sp.exp(a_fun_coef*(self.l0h-self.x)))
@@ -49,6 +52,64 @@ class SCHR:
             Uh = sp.integrate(Fh,(self.x,self.l0h,self.x))
         return Fs,Fh, Us, Uh
     
+    def thelen_03_tendon(self,pomery,koefs,koefh):
+        self.pomery = pomery
+        self.koefs = koefs
+        self.koefh = koefh
+        kh = 1
+        ks = kh*self.pomery
+        fttoe = 0.33
+        kttoes=self.koefs
+        kttoeh=self.koefh
+        epst0=0.04
+        epsttoe =  .99*epst0*np.e**3/(1.66*np.e**3 - .67)
+        # Thorax-scapula
+        epsts = (self.x-self.l0s)/self.l0s
+        Fs = (fttoe/(sp.exp(kttoes)-1)*(sp.exp(kttoes*epsts/epsttoe)-1))*ks
+        
+        # Scapula-humerus
+        epsth = (self.x-self.l0h)/self.l0h
+        Fh = (fttoe/(sp.exp(kttoeh)-1)*(sp.exp(kttoeh*epsth/epsttoe)-1))*kh
+        
+        Us = sp.integrate(Fs,(self.x,self.l0s,self.x))
+        Uh = sp.integrate(Fh,(self.x,self.l0h,self.x))
+        
+        return Fs,Fh,Us,Uh
+    
+    def thelen_03_muscle_pas_force(self,pomery,koefs,koefh,akt_f = True,a_fun_coef=1000):
+        self.pomery = pomery
+        self.koefs = koefs
+        self.koefh = koefh
+        kh = 1
+        ks = kh*self.pomery
+        epsm0=0.6
+        
+        if akt_f:
+            a_funs = 1/(1+sp.exp(a_fun_coef*(self.l0s-self.x)))
+            a_funh = 1/(1+sp.exp(a_fun_coef*(self.l0h-self.x)))
+            lms = self.x/self.l0s
+            Fs = ((sp.exp(self.koefs*(lms-1)/epsm0)-1)/(sp.exp(self.koefs)-1))*ks
+
+            lmh = self.x/self.l0h
+            Fh = ((sp.exp(self.koefh*(lmh-1)/epsm0)-1)/(sp.exp(self.koefh)-1))*kh
+
+            Us_wo = sp.integrate(Fs,(self.x,self.l0s,self.x))
+            Uh_wo = sp.integrate(Fh,(self.x,self.l0h,self.x))
+            Us = a_funs*Us_wo
+            Uh = a_funh*Uh_wo
+        
+        else:
+            lms = self.x/self.l0s
+            Fs = ((sp.exp(self.koefs*(lms-1)/epsm0)-1)/(sp.exp(self.koefs)-1))*ks
+
+            lmh = self.x/self.l0h
+            Fh = ((sp.exp(self.koefh*(lmh-1)/epsm0)-1)/(sp.exp(self.koefh)-1))*kh
+
+            Us = sp.integrate(Fs,(self.x,self.l0s,self.x))
+            Uh = sp.integrate(Fh,(self.x,self.l0h,self.x))
+        
+        return Fs,Fh,Us,Uh
+        
     def potential_energy(self,Us,Uh,xs,xh,dd):
         Us_sub = Us.subs(self.x,xs)
         Uh_sub = Uh.subs(self.x,xh)
@@ -61,7 +122,9 @@ class SCHR:
         d_Uc_np = sp.lambdify([self.phis,self.alfa,self.pomery,self.koefs,self.koefh],d_U_c, 'numpy')
         return Uc_np, d_Uc_np
         
-    def graphs_scapula_position(self,U_C_np,dU_C_np,alfa_start=0.1,alfa_end=140,koefs=2,koefh=3):
+    def graphs_scapula_position(self,U_C_np,dU_C_np,alfa_start=0.1,alfa_end=140,koefs=3,koefh=3,init_root=0.1):
+        self.init_root = init_root
+        
         N = 100
         NG = 10
         alfa_start = alfa_start*np.pi/180
@@ -70,14 +133,13 @@ class SCHR:
         phisvec_root = np.zeros((N,NG))
         phisvec_minimize = np.zeros((N,NG))
         lsvec = np.zeros((N,NG))
-        min_pomer = 0.5
+        min_pomer = 0.1
         max_pomer = 10
         pomery = np.linspace(min_pomer,max_pomer,NG)
 
-        root = 0.1
         bnds = (0, np.pi)
         for j in range(NG):
-            root = 0.8
+            root = self.init_root
             for i in range(N):
 
                 def fun_root(x):
@@ -145,7 +207,8 @@ class SCHR:
                         ## 
 
                         axs[j+len(koefs)*i,a].plot(phis[:,a]*180/np.pi,UC[:,a],label='ks/kh = %s' % round(pomery[k],2))
-                        axs[j+len(koefs)*i,a].title.set_text('koefs = %s, koefh = %s ' % (koefs[i],koefh[j]))  
+                        axs[j+len(koefs)*i,a].title.set_text('koefs = %s, koefh = %s ' % (koefs[i],koefh[j]))
+                        axs[j+len(koefs)*i,a].set_yscale('log')
 
         # lines = []
         # labels = []
